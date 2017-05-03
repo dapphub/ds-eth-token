7f47f49bb0b2bb1704c9636ca45ba63987c38ae6
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2017-05-03 09:53:53 -0300

    Changes for dapp tool

--- src/eth_wrapper.sol
+++ src/eth_wrapper.sol
@@ -1,41 +1,47 @@
-import 'erc20/base.sol';
-import 'actor/base.sol';
+pragma solidity ^0.4.10;
+
+import 'ds-token/base.sol';
+import 'ds-exec/exec.sol';
 
 contract DSEthTokenEvents {
-    event Deposit( address indexed who, uint amount );
-    event Withdrawal( address indexed who, uint amount );
+    event Deposit(address indexed who, uint amount);
+    event Withdrawal(address indexed who, uint amount);
 }
 
-contract DSEthToken is ERC20Base(0)
-                     , DSBaseActor
+contract DSEthToken is DSTokenBase(0)
+                     , DSExec
                      , DSEthTokenEvents
 {
+    string public constant name     = "Wrapper ETH";
+    string public constant symbol   = "W-ETH";
+    uint   public constant decimals = 18;
+
     function totalSupply() constant returns (uint supply) {
         return this.balance;
     }
-    function withdraw( uint amount ) {
+    function withdraw(uint amount) {
         if (!tryWithdraw(amount)) {
             throw;
         }
     }
-    function tryWithdraw( uint amount ) returns (bool ok) {
+    function tryWithdraw(uint amount) returns (bool ok) {
         _balances[msg.sender] = safeSub(_balances[msg.sender], amount);
         bytes memory calldata; // define a blank `bytes`
         if (tryExec(msg.sender, calldata, amount)) { 
-            Withdrawal( msg.sender, amount );
+            Withdrawal(msg.sender, amount);
             return true;
         } else {
             _balances[msg.sender] = safeAdd(_balances[msg.sender], amount);
             return false;
         }
     }
-    function deposit() returns (bool ok) {
+    function deposit() payable {
         _balances[msg.sender] += msg.value;
-        Deposit( msg.sender, msg.value );
-        return true;
+        Deposit(msg.sender, msg.value);
     }
-    function() returns (bool) {
-        return deposit();
+
+    function() payable {
+        deposit();
     }
 
     // Hoisted to remove dependency on entire util package
